b3f573471d9a6a1ab0ff476123bb6ba23e330682
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-03-10 16:52:54 -0400

    Defining types in schema.js file for Apollo.

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,11 +1,15 @@
 import {typeDefs} from '../../../final/client/src/resolvers'
 const { gql } = require('apollo-server')
+// ^ Imports graphql from Apollo Server.
 
 const typeDefs = gql `
  
  type Query {
   launches: [Launch]!
+  # launches => Query for a non-null list of launches that may themselves be null.
   launch(id: ID!): Launch
+  # launch(id: ID!) => Query for a specific launch, based on a non-null ID, that may return
+  # a null launch.
   me: User
  }
 
@@ -38,15 +42,15 @@ type Mission {
 } 
 
 type TripUpdateResponse {
-
+ S
 }
 
 enum PatchSize {
- # Must be defined because it's attached to a Mission field.
- # enum's are custom GQL values that return one of a set of values.
  SMALL
  LARGE
 }
+# enum's are custom GQL values that return one of a set of values.
+# enum PatchSize must be defined because it's attached to a Mission field.
 
 type Mutation {
  bookTrips(launchIds: [ID]!): TripUpdateResponse!
